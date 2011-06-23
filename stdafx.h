801109d82b5ac46a297980de9e45d8e616587ff8
Michał Walenciak <Kicer86@gmail.com>
2011-06-23 14:29:37 +0200

    drobne sprawy porzątkowe

---
+++ stdafx.h
@@ -0,0 +1,15 @@
+ 
+#ifndef STDAFX_H
+#define STDAFX_H
+
+#include <stdlib.h>
+
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <grp.h>
+
+#include <QtGui>
+#include <QtNetwork>
+ 
+#endif
