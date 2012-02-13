a595f4a742449a736563648af41c5add4154cba5
Michal Walenciak <Kicer86@gmail.com>
2012-02-13 21:33:00 +0100

    OpenWithDialog: implementation of Linux' version

--- dialogs/Linux/lin_openwithdialog.hpp
+++ dialogs/Linux/lin_openwithdialog.hpp
@@ -1,13 +1,36 @@
 
-#if defined GUI_TYPE_KDE4
+#ifndef LIN_OPENWITHDIALOG_HPP
+#define LIN_OPENWITHDIALOG_HPP
+
+
+#if defined GUI_TYPE_KDE4 || 1
     #include <kopenwithdialog.h>
+    #include <qtext_choosefile.hpp>
 
+    class OpenWithDialog: public KOpenWithDialog, public QtExtChooseFileDialog
+    {
+        public:
+            virtual int exec()
+            {
+                return KOpenWithDialog::exec();
+            }
 
-    typedef KOpenWithDialog OpenWithDialog;
+            virtual QString result() const
+            {
+                KService::Ptr kservice = KOpenWithDialog::service();
+
+                const QString program = kservice->exec();
+
+                return program;
+            }
+    };
 
 #elif defined GUI_TYPE_GNOME
 
 
 #else
     #error wrong gui choise
+#endif
+
+
 #endif
