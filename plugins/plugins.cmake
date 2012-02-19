5e637305c895aa69e69cd837aa48d45ce8674997
unknown <Michal@virtualbox-winx.(none)>
2012-02-19 11:33:14 +0100

    some functionality common for plugins moved to plugins.cmake

--- plugins/plugins.cmake
+++ plugins/plugins.cmake
@@ -4,3 +4,9 @@ if (WIN32)  # for windows (dll = runtime)
 else (WIN32)
   set(_MODULE LIBRARY)
 endif(WIN32)
+
+#library flags -> hide all symbol, except forced ones
+set(LIB_COMPILE_FLAGS "-std=c++0x -fvisibility=hidden -fvisibility-inlines-hidden")
+
+#allow undefined symbols (some will be taken from builder)
+set(LIB_LINK_FLAGS "-Wl,--allow-shlib-undefined")
